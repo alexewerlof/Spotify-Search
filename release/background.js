54db3efe02bb452b2dceeae56ac91f569910f907
Alex Hanif <hanifbbz@gmail.com>
2012-04-09 17:17:23 +0200

    ready for release 1.0

--- release/background.js
+++ release/background.js
@@ -12,16 +12,6 @@ function openSpotifySearchTab(query) {
 }
 
 /**
- * This event is fired each time the user updates the text in the omnibox as long as the extension's keyword mode is still active.
- */
-chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-	console.log('inputChanged: ' + text);
-	suggest([
-		{content: text, description: "Search for " + text + " on Spotify"}
-	]);
-});
-
-/**
  * This event is fired with the user accepts the input in the omnibox.
  */
 chrome.omnibox.onInputEntered.addListener(function(text) {
